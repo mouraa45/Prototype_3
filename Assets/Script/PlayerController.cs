7529e64a9fc9f3f5d31df81a9fdcb40e85e5ade3
mouraa45 <126695434+mouraa45@users.noreply.github.com>
2023-06-20 15:36:57 +0100

    tags e edicao script player contrloler

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -9,7 +9,7 @@ public class PlayerController : MonoBehaviour
 	public float jumpForce=10;
 	public float gravityModifier;
 	public bool isOnGround=true;
-	
+	public bool gameOver =false;
     // Start is called before the first frame update
     void Start()
     {
@@ -32,7 +32,15 @@ public class PlayerController : MonoBehaviour
       
     private void OnCollisionEnter(Collision collision)
     {
-    	isOnGround = true;
+    	if(collision.gameObject.CompareTag("Ground"))
+    	{
+    		isOnGround=true;
+    	
+    	} else if(collision.gameObject.CompareTag("obstacle"))
+    	{  
+    		gameOver=true;
+    		Debug.Log("Game Over!");
+    	}
     }
 }
     
