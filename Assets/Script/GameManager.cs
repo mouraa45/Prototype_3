950602a54c3abeca34d75933fdebd1f836dbb586
mouraa45 <126695434+mouraa45@users.noreply.github.com>
2023-06-21 03:11:31 +0100

    bonus expert terminada
    boneco começa a correr antes de comelar o jogo

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 	public float score;
+	public Transform startingPoint;
+	public float lerpSpeed;
 	private PlayerController playerControllerScript;
 	
 	
@@ -14,23 +16,54 @@ public class GameManager : MonoBehaviour
 		playerControllerScript =
 			GameObject.Find("Player").GetComponent<PlayerController>();
 		score = 0;
+		
+		playerControllerScript.gameOver = true;
+		StartCoroutine(PlayIntro());
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
 		if(!playerControllerScript.gameOver)
-{
-if(playerControllerScript.doubleSpeed)
-{
-score += 2;
-}
-else
-{
-score++;
-}
-Debug.Log("Score: " + score);
-}
+		{
+			if(playerControllerScript.doubleSpeed)
+			{
+				score += 2;
+			}
+			else
+			{
+				score++;
+			}
+			Debug.Log("Score: " + score);
+		}
 
 	}
+	
+	
+	IEnumerator PlayIntro()
+{
+    Vector3 startPos = playerControllerScript.transform.position;
+    Vector3 endPos = startingPoint.position;
+    float journeyLength = Vector3.Distance(startPos, endPos);
+    float startTime = Time.time;
+
+    playerControllerScript.GetComponent<Animator>().SetFloat("Speed_Multiplier", 0.5f);
+
+    while (true)
+    {
+        float distanceCovered = (Time.time - startTime) * lerpSpeed;
+        float fractionOfJourney = distanceCovered / journeyLength;
+
+        playerControllerScript.transform.position = Vector3.Lerp(startPos, endPos, fractionOfJourney);
+
+        if (fractionOfJourney >= 1)
+            break;
+
+        yield return null;
+    }
+
+    playerControllerScript.GetComponent<Animator>().SetFloat("Speed_Multiplier", 1.0f);
+    playerControllerScript.gameOver = false;
+}
+
 }
