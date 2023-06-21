028572b5d4663d0bd306ada8769d159e0f949512
mouraa45 <126695434+mouraa45@users.noreply.github.com>
2023-06-21 02:48:50 +0100

    hard-- corrida rapida
    feito

--- Assets/Script/MoveLeft.cs
+++ Assets/Script/MoveLeft.cs
@@ -4,29 +4,31 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
-	private float speed=30;
-	private PlayerController playerControllerScript;
-	private float leftBound=-15;
-	
+    private float speed = 30;
+    private PlayerController playerControllerScript;
+    private float leftBound = -15;
+
     // Start is called before the first frame update
     void Start()
     {
-    	playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
-    		
-        
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-    	if(playerControllerScript.gameOver==false)
-    	{
-    	transform.Translate(Vector3.left* Time.deltaTime*speed);
-    	}
-    
-    if(transform.position.x < leftBound  && gameObject.CompareTag("Obstacle"))
-    {
-    	Destroy(gameObject);
-    
-    }}
+        if (!playerControllerScript.gameOver)
+        {
+            float currentSpeed = playerControllerScript.doubleSpeed ? speed * 2 : speed;
+            transform.Translate(Vector3.left * Time.deltaTime * currentSpeed);
+        }
+
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+    }
 }
+
+
+
