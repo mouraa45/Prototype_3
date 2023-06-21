cd882ef5fe67b23b668b28c889ca30b880a3bee2
mouraa45 <126695434+mouraa45@users.noreply.github.com>
2023-06-21 02:21:10 +0100

    forca de salto duplo
    feito

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 	
-	private Rigidbody playerRb; 
+	private Rigidbody playerRb;
 	private Animator playerAnim;
 	private AudioSource playerAudio;
 	public ParticleSystem explosionParticle;
@@ -16,53 +16,65 @@ public class PlayerController : MonoBehaviour
 	public float gravityModifier;
 	public bool isOnGround=true;
 	public bool gameOver;
-	
-    // Start is called before the first frame update
-    void Start()
-    {
-         playerRb = GetComponent<Rigidbody>();
-         playerAnim=GetComponent<Animator>();
-         playerAudio= GetComponent<AudioSource>();
-         Physics.gravity *= gravityModifier;
-         
-         
-    }
+	public bool doubleJumpUsed = false;
+	public float doubleJumpForce;
 
-    // Update is called once per frame
-    void Update()
-    {
-    	if(Input.GetKeyDown(KeyCode.Space)&& isOnGround && !gameOver)
-    	{
-    		playerRb.AddForce(Vector3.up* jumpForce,ForceMode.Impulse);
-    		isOnGround = false;
-    		playerAnim.SetTrigger("Jump_trig");
-    		dirtParticle.Stop();
-    		playerAudio.PlayOneShot(jumpSound, 1.0f);
-    		
-    		
-    	}
-    }
-      
-    private void OnCollisionEnter(Collision collision)
-    {
-    	if(collision.gameObject.CompareTag("Ground"))
-    	{
-    		isOnGround=true;
-    		dirtParticle.Play();
-    	
-    	} else if(collision.gameObject.CompareTag("Obstacle"))
-    	{  
-    		Debug.Log("Game Over!");
-    		gameOver=true;
-    		playerAnim.SetBool("Death_b", true);
-    		playerAnim.SetInteger("DeathType_int",1);
-    		explosionParticle.Play();
-    		dirtParticle.Stop();
-    		playerAudio.PlayOneShot(crashSound, 1.0f);
-    		
-    	}
-    }
+	
+	// Start is called before the first frame update
+	void Start()
+	{
+		playerRb = GetComponent<Rigidbody>();
+		playerAnim=GetComponent<Animator>();
+		playerAudio= GetComponent<AudioSource>();
+		Physics.gravity *= gravityModifier;
+		
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Space)&& isOnGround && !gameOver)
+		{
+			playerRb.AddForce(Vector3.up* jumpForce,ForceMode.Impulse);
+			isOnGround = false;
+			playerAnim.SetTrigger("Jump_trig");
+			dirtParticle.Stop();
+			playerAudio.PlayOneShot(jumpSound, 1.0f);
+			
+			doubleJumpUsed = false;
+		}
+		else if(Input.GetKeyDown(KeyCode.Space) && !isOnGround && !doubleJumpUsed)
+		{
+			doubleJumpUsed = true;
+			playerRb.AddForce(Vector3.up * doubleJumpForce, ForceMode.Impulse);
+			playerAnim.Play("Running_Jump", 3, 0f);
+			playerAudio.PlayOneShot(jumpSound, 1.0f);
+		}
+
+	}
+
+
+private void OnCollisionEnter(Collision collision)
+{
+	if(collision.gameObject.CompareTag("Ground"))
+	{
+		isOnGround=true;
+		dirtParticle.Play();
+		
+	} else if(collision.gameObject.CompareTag("Obstacle"))
+	{
+		Debug.Log("Game Over!");
+		gameOver=true;
+		playerAnim.SetBool("Death_b", true);
+		playerAnim.SetInteger("DeathType_int",1);
+		explosionParticle.Play();
+		dirtParticle.Stop();
+		playerAudio.PlayOneShot(crashSound, 1.0f);
+		
+	}
 }
-    
-    
-    
+}
+	
+	
+	
